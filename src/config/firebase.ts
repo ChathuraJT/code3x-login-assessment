a7a622ec943f9e8996dc0469a192f0181a5ed1c6
Chathura Janaka <chathurajanaka9@gmail.com>
2026-09-19 11:32:55 +0530

    initialize the Firebase app

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHVmo_ain41elRbsEt1T5EJCLhxZ0a2bY",
+  authDomain: "code3x-login-assessment.firebaseapp.com",
+  projectId: "code3x-login-assessment",
+  storageBucket: "code3x-login-assessment.firebasestorage.app",
+  messagingSenderId: "861540278443",
+  appId: "1:861540278443:web:6e49bd16ad13809d5017ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
